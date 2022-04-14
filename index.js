615810102de82c746c1f242a12a0696221213e29
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-04-14 18:31:26 +0430

    parsing url and query string

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const http = require('http');
+const url = require('url')
 
 const server = http.createServer((req,res)=>{
     console.log('server is running');
+    if(req.url === '/favicon.ico') return;
+
+    const myURL = new URL(req.url,`http://${req.headers.host}/`);
+    const pathName = myURL.pathname;
+    const id = myURL.searchParams.get('id');
 
     res.writeHead(200,{'Content-Type' : 'text/html'});
     res.end('<h1>Wellcome to motor shop </h1>');
