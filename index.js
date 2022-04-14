79213cc5b052ba785508dceacc5b297b04d9037c
mohammad rezaei <98229394+mhdrezaei@users.noreply.github.com>
2022-04-14 20:01:01 +0430

    Declaring Routes and Rendering HTML

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const http = require('http');
-const url = require('url')
+const url = require('url');
+const fs = require('fs').promises;
 
-const server = http.createServer((req,res)=>{
+const server = http.createServer(async(req,res)=>{
     console.log('server is running');
     if(req.url === '/favicon.ico') return;
 
     const myURL = new URL(req.url,`http://${req.headers.host}/`);
     const pathName = myURL.pathname;
     const id = myURL.searchParams.get('id');
+
+    if(pathName === '/'){
+        const html = await fs.readFile('./view/shop.html','utf-8');
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.end(html);
+    }else if(pathName === '/product' && id >= 0 && id <= 5){
+        const html = await fs.readFile('./view/product.html','utf-8');
+        res.writeHead(200,{'Content-Type' : 'text/html'});
+        res.end(html);
+    }else{
+        res.writeHead(404,{'Content-Type' : 'text/html'});
+        res.end('page not found');
+    }
+
+    
 
     res.writeHead(200,{'Content-Type' : 'text/html'});
     res.end('<h1>Wellcome to motor shop </h1>');
